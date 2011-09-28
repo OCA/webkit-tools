3cf0465a3cf69646b7a92661d9265dfb95fc934d
unknown <nicolas.bessi@camptocamp.com>
2011-09-28 13:41:55 +0200

    [ADD] report_webkit chapter server helper (lp:c2c-addons/6.1  rev 10)

---
+++ report_webkit_chapter_server/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Camptocamp. Copyright Camptocamp SA
+##############################################################################
+{
+    'name': 'Chapter server helper for report webkit',
+    'version': '0.1',
+    'category': 'Other',
+    'description': """
+      Add chapter_server to report Webkit
+      <% import random %>
+      <%chapter = helper.chapter() %>
+      % for i in xrange(100):
+       <% a = random.randrange(1, 3) %>
+        ${chapter.get_structure(a)}<br/>
+      %endfor
+    """,
+    'author': 'Camptocamp',
+    'website': 'http://openerp.camptocamp.com',
+    'depends': ['report_webkit'],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ report_webkit_chapter_server/report_webkit_chapters.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author Camptocamp. Copyright Camptocamp SA
+##############################################################################
+import logging
+from report_webkit import report_helper
+
+chapter_logger = logging.getLogger('chapter server')
+class chapter_server(object):
+    @classmethod
+    def __init__(self):
+        self.stack = [0]
+
+    @classmethod
+    def get_structure(self, level):
+        while len (self.stack) < level:
+            self.stack.append(0)
+        while len (self.stack) > level:
+            self.stack = self.stack[:level]
+        try:
+            self.stack[len (self.stack)-1] += 1
+        except:
+            chapter_logger.error(self.stack)
+        return ".".join("%s" % s for s in self.stack) + "."
+        
+report_helper.WebKitHelper.chapter = chapter_server
